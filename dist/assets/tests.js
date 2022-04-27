cd75025718a2d5b5d35d8420cb9238a41b88881f
salvo-storyful <salvatore.disalvo@storyful.com>
2022-04-27 15:30:00 +0100

    Deploying your app to Netlify

---
+++ dist/assets/tests.js
@@ -0,0 +1,138 @@
+'use strict';
+
+define("ember-quickstart/tests/helpers/index", ["exports", "ember-qunit"], function (_exports, _emberQunit) {
+  "use strict";
+
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports.setupApplicationTest = setupApplicationTest;
+  _exports.setupRenderingTest = setupRenderingTest;
+  _exports.setupTest = setupTest;
+  0; //eaimeta@70e063a35619d71f0,"ember-qunit"eaimeta@70e063a35619d71f
+
+  // This file exists to provide wrappers around ember-qunit's / ember-mocha's
+  // test setup functions. This way, you can easily extend the setup that is
+  // needed per test type.
+  function setupApplicationTest(hooks, options) {
+    (0, _emberQunit.setupApplicationTest)(hooks, options); // Additional setup for application tests can be done here.
+    //
+    // For example, if you need an authenticated session for each
+    // application test, you could do:
+    //
+    // hooks.beforeEach(async function () {
+    //   await authenticateSession(); // ember-simple-auth
+    // });
+    //
+    // This is also a good place to call test setup functions coming
+    // from other addons:
+    //
+    // setupIntl(hooks); // ember-intl
+    // setupMirage(hooks); // ember-cli-mirage
+  }
+
+  function setupRenderingTest(hooks, options) {
+    (0, _emberQunit.setupRenderingTest)(hooks, options); // Additional setup for rendering tests can be done here.
+  }
+
+  function setupTest(hooks, options) {
+    (0, _emberQunit.setupTest)(hooks, options); // Additional setup for unit tests can be done here.
+  }
+});
+define("ember-quickstart/tests/integration/components/people-list-test", ["@ember/template-factory", "qunit", "ember-qunit", "@ember/test-helpers"], function (_templateFactory, _qunit, _emberQunit, _testHelpers) {
+  "use strict";
+
+  0; //eaimeta@70e063a35619d71f0,"qunit",0,"ember-qunit",0,"@ember/test-helpers",0,"ember-cli-htmlbars"eaimeta@70e063a35619d71f
+
+  (0, _qunit.module)('Integration | Component | people-list', function (hooks) {
+    (0, _emberQunit.setupRenderingTest)(hooks);
+    (0, _qunit.test)('it renders', async function (assert) {
+      // Set any properties with this.set('myProperty', 'value');
+      // Handle any actions with this.set('myAction', function(val) { ... });
+      await (0, _testHelpers.render)((0, _templateFactory.createTemplateFactory)(
+      /*
+        <PeopleList />
+      */
+      {
+        "id": "9zkS59YT",
+        "block": "[[[8,[39,0],null,null,null]],[],false,[\"people-list\"]]",
+        "moduleName": "(unknown template module)",
+        "isStrictMode": false
+      }));
+      assert.dom(this.element).hasText(''); // Template block usage:
+
+      await (0, _testHelpers.render)((0, _templateFactory.createTemplateFactory)(
+      /*
+        
+            <PeopleList>
+              template block text
+            </PeopleList>
+          
+      */
+      {
+        "id": "Zjn5gh9h",
+        "block": "[[[1,\"\\n      \"],[8,[39,0],null,null,[[\"default\"],[[[[1,\"\\n        template block text\\n      \"]],[]]]]],[1,\"\\n    \"]],[],false,[\"people-list\"]]",
+        "moduleName": "(unknown template module)",
+        "isStrictMode": false
+      }));
+      assert.dom(this.element).hasText('template block text');
+    });
+  });
+});
+define("ember-quickstart/tests/test-helper", ["ember-quickstart/app", "ember-quickstart/config/environment", "qunit", "@ember/test-helpers", "qunit-dom", "ember-qunit"], function (_app, _environment, QUnit, _testHelpers, _qunitDom, _emberQunit) {
+  "use strict";
+
+  0; //eaimeta@70e063a35619d71f0,"ember-quickstart/app",0,"ember-quickstart/config/environment",0,"qunit",0,"@ember/test-helpers",0,"qunit-dom",0,"ember-qunit"eaimeta@70e063a35619d71f
+
+  (0, _testHelpers.setApplication)(_app.default.create(_environment.default.APP));
+  (0, _qunitDom.setup)(QUnit.assert);
+  (0, _emberQunit.start)();
+});
+define("ember-quickstart/tests/unit/routes/programmers-test", ["qunit", "ember-qunit"], function (_qunit, _emberQunit) {
+  "use strict";
+
+  0; //eaimeta@70e063a35619d71f0,"qunit",0,"ember-qunit"eaimeta@70e063a35619d71f
+
+  (0, _qunit.module)('Unit | Route | programmers', function (hooks) {
+    (0, _emberQunit.setupTest)(hooks);
+    (0, _qunit.test)('it exists', function (assert) {
+      let route = this.owner.lookup('route:programmers');
+      assert.ok(route);
+    });
+  });
+});
+define("ember-quickstart/tests/unit/routes/scientists-test", ["qunit", "ember-qunit"], function (_qunit, _emberQunit) {
+  "use strict";
+
+  0; //eaimeta@70e063a35619d71f0,"qunit",0,"ember-qunit"eaimeta@70e063a35619d71f
+
+  (0, _qunit.module)('Unit | Route | scientists', function (hooks) {
+    (0, _emberQunit.setupTest)(hooks);
+    (0, _qunit.test)('it exists', function (assert) {
+      let route = this.owner.lookup('route:scientists');
+      assert.ok(route);
+    });
+  });
+});
+define('ember-quickstart/config/environment', [], function() {
+  var prefix = 'ember-quickstart';
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(decodeURIComponent(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+});
+
+require('ember-quickstart/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
